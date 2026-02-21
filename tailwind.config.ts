fc980634ef4626ad07d5f521f5bdb3cc7dce828c
tauso0119 <tauso0119@gmail.com>
2026-02-21 04:43:13 +0000

    fix: Correct tailwind.config.ts module imports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from 'tailwindcss'
-const colors = require('tailwindcss/colors')
+import colors from 'tailwindcss/colors'
+import forms from '@tailwindcss/forms'
 
 const config: Config = {
   content: [
@@ -16,7 +17,7 @@ const config: Config = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    forms,
   ],
 }
 export default config
